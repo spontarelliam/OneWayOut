768ae9e66d3c2bc356aa37a804a9050118dc0503
Adam Spontarelli <adam@vector-space.org>
2016-10-13 15:09:46 +0000

    Removed built-in pull up resistor

--- stop_clock.py
+++ stop_clock.py
@@ -11,13 +11,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 def press_stop():
-    for i in range(3):
+    for i in range(2):
         os.system("irsend SEND_ONCE /home/pi/lircd.conf KEY_STOP")
-        time.sleep(0.1)
+        time.sleep(0.2)
 
-    mp3_file = glob.glob("*.mp3")[0]
+    mp3_file = glob.glob("/home/pi/OneWayOut/*.mp3")[0]
     if mp3_file is not None:
-        play_mp3("/home/pi/OneWayOut/"+mp3_file)
+        play_mp3(mp3_file)
     else:
         print("Error: *.mp3 file not found.")
         return
@@ -25,14 +25,15 @@ def press_stop():
 
 def play_mp3(path):
     print("Playing: " + path)
-    subprocess.Popen(['mpg123', '-q', path])
+    subprocess.Popen(['mpg123', '-q', path]).wait()
     main()
 
 
 def main():
-    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(14, GPIO.IN)
     try:
-        GPIO.wait_for_edge(24, GPIO.FALLING)
+        GPIO.wait_for_edge(14, GPIO.FALLING)
         print("Button Pressed")
         press_stop()
     except KeyboardInterrupt:
